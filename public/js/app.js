6cd50ba41f21e6fdcabf71023a871bad1f26e1e2
Mark Seda <mseda@udel.edu>
2019-01-09 18:59:14 -0500

    Basic sign in is working. Laying down more planning.

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,55 @@
 
 
 
-$("#sign-in-submit-button").click(function (event) {
-    var email = $("sign-in-email-input");
-    var password = $("sign-in-password-input");
+/********** Hande Password Login **********************/
+$("#authentication-login-button").click(function (event) {
+    event.preventDefault();
+    var email = $("#authentication-email-input");
+    var password = $("#authentication-password-input");
+
+    firebase.auth().signInWithEmailAndPassword(email.val(), password.val()).then(function (user) {
+        console.log("Login via username/password successful");
+        $("#authentication-message").text("Register via username/password successful!").show();
+        }).catch(function (error) {
+            console.log("Login via username/password failed")
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log($('#emailInput').val());
+            console.log($('#passwordInput').val());
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            $("#authentication-message").text(errorMessage).show();
+        });
+
+    $("#passwordInput").val('');
 });
 
+$("#authentication-register-button").click(function (event) {
+    event.preventDefault();
+    var email = $("#authentication-email-input");
+    var password = $("#authentication-password-input");
 
-/* Handle User signing in and out */
+    firebase.auth().createUserWithEmailAndPassword(email.val(), password.val()).
+        then(function (user) {
+            console.log("Register via username/password successful");
+            $("#authentication-message").text("Register via username/password successful").show();
+        }).catch(function (error) {
+            console.log("Register via username/password failed.")
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            $("#authentication-message").text(errorMessage).show();
+        });
+
+    $("#passwordInput").val('');
+});
+/*****************************************************/
+
+
+/********** Handle User signing in and out ************/
 firebase.auth().onAuthStateChanged(function (user) {
 
     // user just signed in
@@ -24,12 +66,12 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 function userSignedIn()
 {
-    $(".sign-in").hide();
-    $(".sign-in-failure").hide();
+    //$(".authentication").hide();
 }
 
 function userSignedOut()
 {
-    $(".sign-in").show();
+    $("#authentication-message").hide()
+    $(".authentication").show();
 }
 /******************************************/
