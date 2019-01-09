c61bb6b0c4fdac963c3f5bdc39d3d3c9e563ae51
Mark Seda <mseda@udel.edu>
2019-01-08 20:50:36 -0500

    Began sign in logic

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,35 @@
-$(".sign-in-failure").show();
 
-$(".sign-in-failure").hide();
+
+
+$("#sign-in-submit-button").click(function (event) {
+    var email = $("sign-in-email-input");
+    var password = $("sign-in-password-input");
+});
+
+
+/* Handle User signing in and out */
+firebase.auth().onAuthStateChanged(function (user) {
+
+    // user just signed in
+    if (user) {
+
+        userSignedIn();
+    }
+    else
+    {
+        userSignedOut();
+    }
+});
+
+
+function userSignedIn()
+{
+    $(".sign-in").hide();
+    $(".sign-in-failure").hide();
+}
+
+function userSignedOut()
+{
+    $(".sign-in").show();
+}
+/******************************************/
